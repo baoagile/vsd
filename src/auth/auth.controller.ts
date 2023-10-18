2e92b90ba4d3c286ae997385d66faafc59775301
baoagile <ngoc.hb@agiletechsoftware.com>
2023-10-18 18:08:30 +0700

    update dóc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,48 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiConsumes,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginDto, RegisterDto } from './auth.dto';
+import { AuthResponse, LoginDto, RegisterDto } from './auth.dto';
 
+@ApiTags('auth')
+@ApiConsumes('application/json')
+@UsePipes(
+  new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }),
+)
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ description: 'Login user' })
+  @ApiOkResponse({
+    type: AuthResponse,
+    description: 'fetched success',
+  })
   @Post('login')
   login(@Body() data: LoginDto) {
     return this.authService.login(data);
   }
 
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ description: 'Register user' })
   @Post('register')
   register(@Body() data: RegisterDto) {
     return this.authService.register(data);
